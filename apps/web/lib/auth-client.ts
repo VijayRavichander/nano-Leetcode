d65fc81088527ec8a830baed2752617310bb8aa2
Vijay Ravichander <vijaypreetham1@gmail.com>
2025-10-16 09:03:35 -0700

    stuff

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -4,6 +4,8 @@ const BASE_URL = process.env.NODE_ENV === 'production'
   ? "https://litecode.vijayravichander.com" 
   : "http://localhost:3000";
 
+
+
 export const authClient = createAuthClient({
-  baseURL: BASE_URL,
+  baseURL: "https://litecode.vijayravichander.com",
 });
